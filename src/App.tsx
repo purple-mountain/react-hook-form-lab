712070a617adeee5de83546aabd6b2c6a49d86bd
purple-mountain <lmwrong92@gmail.com>
2024-09-30 14:24:29 +0000

    add user form with zod validation and react hook form

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { z } from "zod";
+import "./App.css";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+const userSchema = z.object({
+  firstName: z
+    .string()
+    .min(1, "First name is required")
+    .max(100, "Name cannot be more than 50 characters"),
+  lastName: z
+    .string()
+    .max(100, "Last Name cannot be more than 50 aracters")
+    .min(1, "Last name is required"),
+  email: z.string().email("Invalid Email"),
+});
+
+type UserSchema = z.infer<typeof userSchema>;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {
+    handleSubmit,
+    register,
+    reset,
+    formState: { errors },
+  } = useForm<UserSchema>({
+    resolver: zodResolver(userSchema),
+  });
+
+  function onSubmit(data: UserSchema) {
+    console.log(data);
+    reset();
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <main className="flex w-screen h-screen justify-center items-center flex-col space-y-2">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col space-y-2"
+      >
+        <input
+          {...register("firstName")}
+          className="border border-black round"
+          placeholder="First Name"
+        />
+        <p className="text-red-700">{errors.firstName?.message}</p>
+
+        <input
+          {...register("lastName")}
+          className="border border-black round"
+          placeholder="Last Name"
+        />
+        <p className="text-red-700">{errors.lastName?.message}</p>
+
+        <input
+          {...register("email")}
+          className="border border-black round"
+          placeholder="Email"
+        />
+        <p className="text-red-700">{errors.email?.message}</p>
+
+        <button type="submit" className="border border-black">
+          Submit
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </form>
+    </main>
+  );
 }
 
-export default App
+export default App;
